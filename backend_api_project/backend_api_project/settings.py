a66fe308abb59d33c15b8695cbd5ff45e59f6c3d
n-rdy <giuseppegelardi@icloud.com>
2021-09-14 14:47:35 -0500

    created registration serializer class, register view class for auth app

--- backend_api_project/backend_api_project/settings.py
+++ backend_api_project/backend_api_project/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'authentication.apps.AuthenticationConfig',
 ]
 
 MIDDLEWARE = [
@@ -110,6 +112,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
 
 try: 
     from backend_api_project.local_settings import *
