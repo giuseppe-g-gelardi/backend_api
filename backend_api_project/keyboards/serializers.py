329ade42c1094253eb3ef173ef6747e94a232d0c
n-rdy <giuseppegelardi@icloud.com>
2021-09-17 13:15:43 -0500

    droped and restarted database, linked user as an FK to keyboards

--- backend_api_project/keyboards/serializers.py
+++ backend_api_project/keyboards/serializers.py
@@ -1,9 +1,7 @@
 from rest_framework import serializers
-from .models import User
 from .models import Keyboards
-
+ 
 class KeyboardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Keyboards
-        fields = ['id', 'layout', 'angle', 'body_material', 'body_finish_alu', 'body_finish_pc', 'plate_material', 'plate_finish_alu', 'plate_finish_brass', 'plate_finish_pc', 'weight_material', 'weight_finish_all', 'mounting_style']
-
+        fields = ['id', 'layout', 'color', 'keycap_color', 'user']
