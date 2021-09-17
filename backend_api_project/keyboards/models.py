6376c0711e13530c9a95324e8c254a4caee7fb64
n-rdy <giuseppegelardi@icloud.com>
2021-09-17 14:59:14 -0500

    fixed migrations and database. trying to fix endpoints for keyboards

--- backend_api_project/keyboards/models.py
+++ backend_api_project/keyboards/models.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.models import User
 from django.db import models
- 
+from django.contrib.auth.models import User
+
 class Keyboards(models.Model):
-    layout = models.CharField(max_length=50),
-    color = models.CharField(max_length=50),
-    keycap_color = models.CharField(max_length=50),
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default =None)
+    layout = models.CharField(max_length=50, default=None)
+    color = models.CharField(max_length=50, default=None)
+    keycap_color = models.CharField(max_length=50, default=None)
+
     
