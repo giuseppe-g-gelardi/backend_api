291d767708843ce9782d8526d1535a25952fe01c
n-rdy <giuseppegelardi@icloud.com>
2021-09-17 11:53:41 -0500

    added keyboards as foreignkey and added models and views for keyboards

--- backend_api_project/keyboards/models.py
+++ backend_api_project/keyboards/models.py
@@ -1,143 +1,45 @@
 from django.db import models
 
-
-# specifying choices
-LAYOUT_CHOICES = (
-    ('1', '40%'),
-    ('2', '60%'),
-    ('3', '65%'),
-    ('4', '75%'),
-    ('5', 'TKL'),
-    ('6', 'Full-size'),
-    ('7', 'Other'),
-),
-
-BODY_MATERIAL_CHOICES = (
-    ('1', 'Aluminum'),
-    ('2', 'Polycarbonate'),
-),
-
-BODY_FINISH_ALU_CHOICES = (
-    ('1', 'Anodized'),
-    ('2', 'Cerakote'),
-    ('3', 'E-Coat'),
-),
-
-BODY_FINISH_PC_CHOICES = (
-    ('1', 'Clear'),
-    ('2', 'Frosted'),
-    ('3', 'Dyed'),
-),
-
-PLATE_MATERIAL_CHOICES = (
-    ('1', 'Aluminum'),
-    ('2', 'Brass'),
-    ('3', 'Polycarbonate'),
-    ('4', 'POM'),
-    ('5', 'Carbon Fiber'),
-),
-
-PLATE_FINISH_ALU_CHOICES = (
-    ('1', 'Anodized'),
-    ('2', 'Bead Blasted'),
-),
-
-PLATE_FINISH_BRASS_CHOICES = (
-    ('1', 'Polished'),
-    ('2', 'Bead Blasted'),
-),
-
-PLATE_FINISH_PC_CHOICES = (
-    ('1', 'Clear'),
-    ('2', 'Frosted'),
-),
-
-WEIGHT_MATERIAL_CHOICES = (
-    ('1', 'Brass'),
-    ('2', 'Stainless Steel'),
-    ('3', 'None'),
-),
-
-WEIGHT_FINISH_CHOICES = (
-    ('1', 'Polished'),
-    ('2', 'Bead Blasted'),
-    ('3', 'PVD'),
-),
-
-MOUNTING_STYLE_CHOICES = (
-    ('1', 'Gasket (Isolation)'),
-    ('2', 'Gasket (O-Ring)'),
-    ('3', 'Top Mount'),
-    ('4', 'Other'),
-),
-
-
-# declaring models
 class Keyboards(models.Model):
-    layout = models.CharField(
-        max_length=25,
-        choices=LAYOUT_CHOICES,
-        default='2',
-    ),
-    angle = models.CharField(max_length=25),
+    layout = models.CharField(max_length=50),
+    color = models.CharField(max_length=50),
+    keycap_color = models.CharField(max_length=50),
 
-    body_material = models.CharField(
-        max_length=25,
-        choices=BODY_MATERIAL_CHOICES,
-        default='1',
-    ),
 
-    body_finish_alu = models.CharField(
-        max_length=25,
-        choices=BODY_FINISH_ALU_CHOICES,
-        default='1',
-    ),
-
-    body_finish_pc = models.CharField(
-        max_length=25,
-        choices=BODY_FINISH_PC_CHOICES,
-        default='2'
-    ),
-
-    plate_material = models.CharField(
-        max_length=25,
-        choices=PLATE_MATERIAL_CHOICES,
-        default=5,
-    ),
-
-    plate_finish_alu = models.CharField(
-        max_length=25,
-        choices=PLATE_FINISH_ALU_CHOICES,
-        default='1',
-    ),
-
-    plate_finish_brass = models.CharField(
-        max_length=25,
-        choices=PLATE_FINISH_BRASS_CHOICES,
-        default='1',
-    ),
-
-    plate_finish_pc = models.CharField(
-        max_length=25,
-        choices=PLATE_FINISH_PC_CHOICES,
-        default=2,
-    ),
-
-    weight_material = models.CharField(
-        max_length=25,
-        choices=WEIGHT_MATERIAL_CHOICES,
-        default=2,
-    ),
-
-    weight_finish = models.CharField(
-        max_length=25,
-        choices=WEIGHT_FINISH_CHOICES,
-        default=3,
-    ),
-
-    mounting_style = models.CharField(
-        max_length=25,
-        choices=MOUNTING_STYLE_CHOICES,
-        default='1',
-    ),
+# LAYOUT_CHOICES = (
+#     ('1', '60%'),
+#     ('2', 'alice'),
+#     ('3', 'TKL'),
     
+# ),
+
+# COLOR_CHOICES = (
+#     ('1', 'obsidian'),
+#     ('2', 'ewhite'),
+#     ('3', 'polycarb'),
+# ),
+
+# KEYCAP_COLOR_CHOICES = (
+#     ('1', 'dracula'),
+#     ('2', 'muted'),
+#     ('3', 'futurefunk')
+# )
+
+# class Keyboards(models.Model):
+#     layout = models.CharField(
+#         max_length=50,
+#         choices=LAYOUT_CHOICES,
+#         default='1',
+#     ),
+
+#     color = models.CharField(
+#         max_length=50,
+#         choices=COLOR_CHOICES,
+#         default='1', 
+#     ),
+
+#     keycap_color = models.CharField(
+#         max_length=50,
+#         choices=KEYCAP_COLOR_CHOICES,
+#         default=1,
+#     ),
--- backend_api_project/users/models.py
+++ backend_api_project/users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-# from django.contrib.auth.models import User
+from keyboards.models import Keyboards
 
 
 class User(models.Model):
@@ -8,4 +8,4 @@ class User(models.Model):
     email = models.CharField(max_length=100)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-
+    keyboard = models.ForeignKey(Keyboards, on_delete=models.CASCADE)
